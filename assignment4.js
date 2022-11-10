c450c0d90e3e3349e251623ae900e634a48b8814
Anica Wang <anicawang@gmail.com>
2022-11-10 00:39:48 -0800

    A4 final

--- assignment4.js
+++ assignment4.js
@@ -23,8 +23,13 @@ export class Assignment4 extends Scene {
             box_2: new Cube(),
             axis: new Axis_Arrows()
         }
-        console.log(this.shapes.box_1.arrays.texture_coord)
-
+        this.shapes.box_2.arrays.texture_coord.forEach(
+            (v, i, l) => {
+                v[0] = v[0] * 2
+                v[1] = v[1] * 2
+            }
+        )
+        console.log(this.shapes.box_2.arrays.texture_coord)
 
         // TODO:  Create the materials required to texture both cubes with the correct images and settings.
         //        Make each Material from the correct shader.  Phong_Shader will work initially, but when
@@ -33,18 +38,31 @@ export class Assignment4 extends Scene {
             phong: new Material(new Textured_Phong(), {
                 color: hex_color("#ffffff"),
             }),
-            texture: new Material(new Textured_Phong(), {
-                color: hex_color("#ffffff"),
-                ambient: 0.5, diffusivity: 0.1, specularity: 0.1,
-                texture: new Texture("assets/stars.png")
+            texture_1: new Material(new Texture_Rotate(), {
+                color: hex_color("#000000"),
+                ambient: 1, diffusivity: 0.1, specularity: 0.1,
+                texture: new Texture("assets/stars.png", "NEAREST")
+            }),
+            texture_2: new Material(new Texture_Scroll_X(), {
+                color: hex_color("#000000"),
+                ambient: 1, diffusivity: 0.1, specularity: 0.1,
+                texture: new Texture("assets/earth.gif", "LINEAR_MIPMAP_LINEAR")
             }),
         }
 
         this.initial_camera_location = Mat4.look_at(vec3(0, 10, 20), vec3(0, 0, 0), vec3(0, 1, 0));
+        
+        this.box1_transform = Mat4.translation(-2,0,0);
+        this.box2_transform = Mat4.translation(2,0,0);
+
+        this.rotate = false
     }
 
     make_control_panel() {
         // TODO:  Implement requirement #5 using a key_triggered_button that responds to the 'c' key.
+        this.key_triggered_button("Cube rotation", ["c"], () => {
+            this.rotate = !this.rotate; 
+        });
     }
 
     display(context, program_state) {
@@ -64,8 +82,20 @@ export class Assignment4 extends Scene {
         let model_transform = Mat4.identity();
 
         // TODO:  Draw the required boxes. Also update their stored matrices.
-        // You can remove the folloeing line.
-        this.shapes.axis.draw(context, program_state, model_transform, this.materials.phong.override({color: hex_color("#ffff00")}));
+        // this.shapes.axis.draw(context, program_state, model_transform, this.materials.phong.override({color: hex_color("#ffff00")}));
+        
+        // rotation - 20rpm = 20* 2 * pi deg/min
+        // 40pi / 60sec = 2pi/3 per sec
+
+        let box1_angle = (2. / 3.) * Math.PI * dt;
+        this.box1_transform = this.rotate ? this.box1_transform.times(Mat4.rotation(box1_angle, 1, 0, 0)) : this.box1_transform;
+
+        let box2_angle = (2. / 3.) * Math.PI * dt;
+        this.box2_transform = this.rotate ? this.box2_transform.times(Mat4.rotation(box2_angle, 0, 1, 0)) : this.box2_transform;
+
+        // Cube 1
+        this.shapes.box_1.draw(context, program_state, this.box1_transform, this.materials.texture_1);
+        this.shapes.box_2.draw(context, program_state, this.box2_transform, this.materials.texture_2);
     }
 }
 
@@ -79,8 +109,23 @@ class Texture_Scroll_X extends Textured_Phong {
             uniform float animation_time;
             
             void main(){
-                // Sample the texture image in the correct place:
-                vec4 tex_color = texture2D( texture, f_tex_coord);
+                float dx = mod((2.0 * animation_time), 1.0);
+                float u = abs(mod(f_tex_coord.x - dx, 1.0) - 0.5);
+                float v = abs(mod(f_tex_coord.y, 1.0) - 0.5);
+                
+                vec2 translated_tex_coord = vec2(f_tex_coord.x - dx, f_tex_coord.y);
+                vec4 tex_color;
+
+                if ( 
+                    (u > 0.25 && u < 0.35 && v < 0.35) ||
+                    (v > 0.25 && v < 0.35 && u < 0.35)
+                ) {
+                    tex_color = vec4(0, 0, 0, 1.0);
+                } else {                
+                    // Sample the texture image in the correct place:
+                    tex_color = texture2D( texture, translated_tex_coord);
+                }
+
                 if( tex_color.w < .01 ) discard;
                                                                          // Compute an initial (ambient) color:
                 gl_FragColor = vec4( ( tex_color.xyz + shape_color.xyz ) * ambient, shape_color.w * tex_color.w ); 
@@ -90,7 +135,7 @@ class Texture_Scroll_X extends Textured_Phong {
     }
 }
 
-
+// 15 rpm = (30 * pi) / 60s) = (pi / 4)
 class Texture_Rotate extends Textured_Phong {
     // TODO:  Modify the shader below (right now it's just the same fragment shader as Textured_Phong) for requirement #7.
     fragment_glsl_code() {
@@ -100,7 +145,37 @@ class Texture_Rotate extends Textured_Phong {
             uniform float animation_time;
             void main(){
                 // Sample the texture image in the correct place:
-                vec4 tex_color = texture2D( texture, f_tex_coord );
+                float pi = 3.14159;
+                float angle = mod(pi * animation_time / 2.0, 2.0 * pi);
+
+                float u = f_tex_coord.x - 0.5;
+                float v = f_tex_coord.y - 0.5;
+                
+                float s = sin(angle);
+                float c = cos(angle);
+
+                mat2 m = mat2(
+                    c, -s,
+                    s, c
+                );
+                
+                vec4 tex_color;
+                vec2 new_coord = m * vec2(u, v);
+                vec2 translated_tex_coord = vec2(new_coord.x + 0.5, new_coord.y + 0.5);
+
+                u = abs(new_coord.x);
+                v = abs(new_coord.y);
+
+                if ( 
+                    (u > 0.25 && u < 0.35 && v < 0.35) ||
+                    (v > 0.25 && v < 0.35 && u < 0.35)
+                ) {
+                    tex_color = vec4(0, 0, 0, 1.0);
+                } else {                
+                    // Sample the texture image in the correct place:
+                    tex_color = texture2D( texture, translated_tex_coord);
+                }
+
                 if( tex_color.w < .01 ) discard;
                                                                          // Compute an initial (ambient) color:
                 gl_FragColor = vec4( ( tex_color.xyz + shape_color.xyz ) * ambient, shape_color.w * tex_color.w ); 
